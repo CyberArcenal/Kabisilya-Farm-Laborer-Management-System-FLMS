f5c412551f7c6b0ba56d3e3e5d28209d51bc0b64
CyberArcenal <148480765+CyberArcenal@users.noreply.github.com>
2026-06-12 04:23:41 +0800

    Update systemSettings.js

--- src/entities/systemSettings.js
+++ src/entities/systemSettings.js
@@ -3,12 +3,14 @@ const { EntitySchema } = require("typeorm");
 
 const SettingType = {
   GENERAL: "general",
-  COLLECTIONS: "collections",
-  LOANS: "loans",
-  NOTIFICATIONS: "notifications",
-  REPORTS: "reports",
-  INTEGRATIONS: "integrations",
-  AUDIT_SECURITY: "audit_security",
+  NOTIFICATIONS: "notification",
+  FARM_SESSION: "farm_session",
+  FARM_BUKID: "farm_bukid",
+  FARM_PITAK: "farm_pitak",
+  FARM_ASSIGNMENT: "farm_assignment",
+  FARM_PAYMENT: "farm_payment",
+  FARM_DEBT: "farm_debt",
+  FARM_AUDIT: "farm_audit",
 };
 
 const SystemSetting = new EntitySchema({
